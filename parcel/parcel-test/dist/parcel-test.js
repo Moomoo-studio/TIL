d1d8522d63bd59fe402fe517ec11f2af605eb2ff
dvmoomoodv <dvmoomoodv@gmail.com>
2018-02-25 21:00:42 +0900

    push

--- parcel/parcel-test/dist/parcel-test.js
+++ parcel/parcel-test/dist/parcel-test.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({14:[function(require,module,exports) {
+})({5:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],13:[function(require,module,exports) {
+},{}],4:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,19 +132,19 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":14}],12:[function(require,module,exports) {
+},{"./bundle-url":5}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":13}],4:[function(require,module,exports) {
+},{"_css_loader":4}],2:[function(require,module,exports) {
 "use strict";
 
 require("./styles.scss");
 
 console.log("hello world");;
-},{"./styles.scss":12}],18:[function(require,module,exports) {
+},{"./styles.scss":3}],14:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -164,7 +164,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '51471' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '57332' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -265,5 +265,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[18,4])
+},{}]},{},[14,2])
 //# sourceMappingURL=/dist/parcel-test.map
